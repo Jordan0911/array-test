ac161157cbcbd80d904a99dc9db1a0438184dd15
Your friendly neighborhood programmer <colombo.jordan@franchettisalviani.net>
2025-11-03 08:56:48 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace array_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            int[] numbers = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    numbers[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("|" + numbers[i] + "|");
+                }
+        }
+    }
+
+}
