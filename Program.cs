921ef4b7b3c3c9525234b74af284c1bee0c58902
Your friendly neighborhood programmer <colombo.jordan@franchettisalviani.net>
2025-11-10 08:48:29 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,62 +4,84 @@
     {
         static void Main(string[] args)
         {
-        int odd_counter = 0;    
-        int even_counter = 0; 
-        int sum = 0;
-        double average = 0;
-        int min = 100000000; 
-        int max=0;
-        int t=0;
-        int s = 0;
 
+            int odd_counter = 0;
+            int even_counter = 0;
+            int sum = 0;
+            double average = 0;
+            int min = 100000000;
+            int max = 0;
+            int t = 0;
+            int s = 0;
+            int inside_ab_counter = 0;
+            int outside_ab_counter = 0;
+            int inside_cd_counter = 0;
+            int outside_cd_counter = 0;
+           
             Console.WriteLine("Enter lenght of array ");
             int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter x ");  
-            int x= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x ");
+            int x = Convert.ToInt32(Console.ReadLine());
             int[] numbers = new int[n];
-            int[] placeholder = new int[n];
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("Enter interval limits"); 
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second interval limits");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            // input array elements 
+            for (int i = 0; i < numbers.Length; i++)
             {
-                    numbers[i] = Convert.ToInt32(Console.ReadLine());
-                    if (min>=numbers[i])
-                    {
-                        min = numbers[i];   
-                    }
-                        else if(numbers[i] >= max)
-                        { 
-                            max = numbers[i]; 
-                        }
-                            else if(numbers [i]>x)
-                            {
-                                t++;
-                                
-                    
-                            }
-                                else if (numbers[i] < x)
-                                {
-                                    s++;
-                                }
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
 
+            }
+            // calculate min, max and sum
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (min >= numbers[i])
+                {
+                    min = numbers[i];
+                }
+                else if (numbers[i] >= max)
+                {
+                    max = numbers[i];
+                }
                 sum += numbers[i];
             }
-                // first exercise
-                for (int i = 0; i < n; i++)
-                {
-                Console.WriteLine($"[ {numbers[i]} ]");
-                }
-           
-            // second exercise
-            for (int i = 0; i < n; i++)
+            // setting lengths of greater than x and smaller than x arrays
+            for (int i = 0; i < numbers.Length; i++)
             {
-                if(i % 2 == 0)
+                if (numbers[i] > x)
                 {
-                 Console.WriteLine($"[ {numbers[i]} ]");
+                    t++;
+
+                }
+                else if (numbers[i] <= x)
+                {
+                    s++;
                 }
             }
-          
+            // first exercise
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"[ {numbers[i]} ]");
+            }
+
+            // second exercise
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine($"[ {numbers[i]} ]");
+                }
+            }
+
             // third exercise   
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
                 if (i % 2 != 0)
                 {
@@ -69,45 +91,76 @@
 
             // fourth exercise
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
                 if (i % 2 == 0)
                 {
-                   even_counter++;
+                    even_counter++;
                 }
                 else
                 {
-                   odd_counter++;
+                    odd_counter++;
                 }
             }
-            Console.WriteLine("there are "+odd_counter  +" odd numbers and " +even_counter+" even numbers");
+            Console.WriteLine("there are " + odd_counter + " odd numbers and " + even_counter + " even numbers");
             //fifth exercise
             average = sum / n;
-            Console.WriteLine("the minimum is " + min + " the maximum is "+max+" and the average is " + average);
+            Console.WriteLine("the minimum is " + min + " the maximum is " + max + " and the average is " + average);
             // sixth exercise
             int[] greater_than_x = new int[t];
             int[] smaller_than_x = new int[s];
-            for(int i = 0; i < n; i++)
+            int t2 = 0;
+            int s2 = 0;
+            for (int i = 0; i < numbers.Length; i++)
             {
-                if(numbers[i] > x)
+                if (numbers[i] > x)
                 {
-                    greater_than_x[i] = numbers[i];
+                    greater_than_x[t2] = numbers[i];
+                    t2++;
                 }
-                else if (numbers[i] < x)
+                else if (numbers[i] <= x)
                 {
-                    smaller_than_x[i] = numbers[i];
+                    smaller_than_x[s2] = numbers[i];
+                    s2++;
                 }
             }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i]<=b && numbers[i]>=a)
+                {
+                    inside_ab_counter++;                 
+                }
+                else if(numbers[i]<a || numbers[i]>b)   
+                {
+                    outside_ab_counter++;
+                }
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] <= d && numbers[i] >= c)
+                {
+                    inside_ab_counter++;
+                }
+                else if (numbers[i] < c || numbers[i] > d)
+                {
+                    outside_ab_counter++;
+                }
+            }
+            Console.WriteLine("numbers smaller than or equal to x are: ");
+            for (int i = 0; i < smaller_than_x.Length; i++)
+            {
+                Console.WriteLine($"[ {smaller_than_x[i]} ]");
+            }
+
             Console.WriteLine("numbers greater than x are: ");
-            for(int i = 0; i < t; i++)
+            for (int i = 0; i < greater_than_x.Length; i++)
             {
-                Console.WriteLine(greater_than_x[i]);
+                Console.WriteLine($"[ {greater_than_x[i]} ]");
             }
-            Console.WriteLine("numbers smaller than x are: ");  
-            for (int i = 0; i < s; i++)
-            {
-                Console.WriteLine(smaller_than_x[i]);
-            }
+            Console.WriteLine("there are " + inside_ab_counter + " numbers inside the second interval and " + outside_ab_counter + " numbers outside the first interval");
+            Console.WriteLine("there are " + inside_cd_counter + " numbers inside the second interval and " + outside_cd_counter + " numbers outside the second interval");
+
+
         }
     }
 
